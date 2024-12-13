46300efcfe543ed830fb26c81a9b5d859b316e01
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-12-13 17:30:25 +0100

    feat: page top1

---
+++ src/components/home-popular.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+const props = defineProps(['top1'])
+console.log(props)
+
+</script>
+
+
+<template>
+
+  <h2>Drama | Thriller | Supernatural</h2>
+  <h1>Stranger Things</h1>
+  <div>
+    <p>2019</p>
+    <p><span>Directeur</span> Shawn Levy</p>
+    <p><span>Saisons</span>3</p>
+  </div>
+  <p> In 1980s Indiana, a group of young friends witness supernatural forces and secret government exploits. As they search for answers, the children unravel a series of extraordinary mysteries.</p>
+  <p>Note : 5</p>
+  <button>
+    Regarder
+  </button>
+  <button>
+    Tous les épisodes
+  </button>
+
+
+</template>
+
+<style scoped>
+
+</style>
